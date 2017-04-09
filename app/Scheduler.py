7bb79488f7d69b33b6f5e66738dd6b68e0b87ab8
Steeve Vandecappelle <me@mizore.fr>
2017-04-09 19:50:23 +0200

    daemonsize config parser using pyinotify

--- app/Scheduler.py
+++ app/Scheduler.py
@@ -2,42 +2,53 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import ConfigParser
+import configparser
 import os
 import importlib
 import json
 
+import pyinotify
+import asyncio
+from multiprocessing import Process, Queue, TimeoutError
+    
 class Scheduler:
     """Scheduler is the main entry for launch deferred scheduled tasks"""
 
     def __init__(self):
         """Initialize a Scheduler task launcher"""
         self.logger = logging.getLogger('Scheduler')
-        self.config = ConfigParser.ConfigParser()
-        self.modulesConfiguration = ConfigParser.ConfigParser()
+        self.config = configparser.ConfigParser()
+        self.modulesConfiguration = configparser.ConfigParser()
         self.modules = []
 
-    def launch(self):
+    def launch(self, daemon=False):
         """Launch the scheduled tasks configured"""
         self.logger.info("Loading tasks configured")
         self.configure()
         self.scan()
-        moduleFileConfig = "%s/%s" % (self.modulesLocation, "modules.conf")
-        self.logger.info("Loading module configuration: %s" % moduleFileConfig)
-        self.modulesConfiguration.read(moduleFileConfig)
+        
+        if daemon:
+            self.logger.info("Starting configurator watcher service")
+            self.watcher.watch()
+        else:
+            moduleFileConfig = "%s/%s" % (self.modulesLocation, "modules.conf")
+            self.logger.info("Loading module configuration: %s" % moduleFileConfig)
+            self.modulesConfiguration.read(moduleFileConfig)
 
-        moduleLaunchOrderFile = self.modulesConfiguration.get("general", "order")
-        self.logger.info("Order json file: %s" % moduleLaunchOrderFile)
-
-        self.moduleLaunchOrder = json.load(open("%s/%s" % (self.modulesLocation, moduleLaunchOrderFile)))["actives"]
-        for module in self.moduleLaunchOrder:
-            TaskLoader(self.modulesLocation).load(module)
+            moduleLaunchOrderFile = self.modulesConfiguration.get("general", "order")
+            self.logger.info("Order json file: %s" % moduleLaunchOrderFile)
+            
+            self.logger.info("Load all tasks configured")
+            self.moduleLaunchOrder = json.load(open("%s/%s" % (self.modulesLocation, moduleLaunchOrderFile)))["actives"]
+            for module in self.moduleLaunchOrder:
+                TaskLoader(self.modulesLocation).load(module)
 
     def configure(self):
         self.config.read("junxia.conf")
 
     def scan(self):
         self.modulesLocation = self.config.get("modules", "location")
+        self.watcher = ConfigWatcher(self.modulesLocation)
         self.logger.info("Scan for modules stored into the folder: %s" % (self.modulesLocation))
 
         for module in os.listdir(self.modulesLocation):
@@ -58,3 +69,32 @@ class TaskLoader:
         modulePackage = importlib.import_module("%s.%s" % (self.moduleLocation, module))
         runner = modulePackage.Runner()
         runner.run()
+
+class ConfigWatcher:
+
+    def __init__(self, scan_folder):
+        """
+        """
+        self.scan_folder = scan_folder
+
+    def handle_read_callback(self, notifier):
+        """
+        Just stop receiving IO read events after the first
+        iteration (unrealistic example).
+        """
+        print('handle_read callback')
+        notifier.loop.stop()
+    
+    def scanner(self):
+        wm = pyinotify.WatchManager()
+        loop = asyncio.get_event_loop()
+        notifier = pyinotify.AsyncioNotifier(wm, loop,
+                                            callback=self.handle_read_callback)
+        wm.add_watch(self.scan_folder, pyinotify.ALL_EVENTS)
+        loop.run_forever()
+        notifier.stop()
+
+    def watch(self):
+        self.watcher_process_queue = Queue()
+        self.process = Process(target=self.scanner, args=())
+        self.process.start()
--- run.py
+++ run.py
@@ -2,11 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import sys, getopt
 
 from settings.logger import LoggerConfigurator
 from app.Scheduler import Scheduler
+from app.Scheduler import ConfigWatcher
 
-if __name__ == "__main__":
+def main(argv):
+    daemon = False
+    try:
+        opts, args = getopt.getopt(argv,"h",["daemon"])
+    except getopt.GetoptError:
+        print('test.py [-d] [--daemon]')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py [-d] [--daemon]')
+            sys.exit()
+        elif opt in ("-d", "--daemon"):
+            daemon = True
     logger = logging.getLogger('junxia')
 
     loggerConfigurator = LoggerConfigurator()
@@ -14,4 +28,8 @@ if __name__ == "__main__":
 
     logger.info('Launching junxia: %s', 'start scheduled tasks')
     schedulerRunner = Scheduler()
-    schedulerRunner.launch()
+    schedulerRunner.launch(daemon=daemon)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    
